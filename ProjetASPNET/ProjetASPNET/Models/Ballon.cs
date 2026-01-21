f8a61d555058503e4aa38782c0d0fe673c5331cb
resox rx <uagoinfo@gmail.com>
2026-01-21 22:28:15 +0200

    JORDAN BATEYE

---
+++ ProjetASPNET/ProjetASPNET/Models/Ballon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class Ballon
+{
+    // USHINDI BATEYE JORDAN
+
+    public string Couleur { get; set; }
+    public double Taille { get; set; } // diamètre en cm
+    public bool EstGonfle { get; private set; }
+
+
+    public Ballon(string couleur, double taille)
+    {
+        Couleur = couleur;
+        Taille = taille;
+        EstGonfle = false;
+    }
+
+}
