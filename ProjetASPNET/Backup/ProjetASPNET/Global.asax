fed57adce6b9136ebe6a0d95c9e79143681d7b35
L3InfoUAGO <uagoinfo@gaiml.com>
2026-01-21 13:41:37 +0100

    bonjour moi billy qui vient d'ajouter les codes en classe or  ici bien

---
+++ ProjetASPNET/Backup/ProjetASPNET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjetASPNET.MvcApplication" Language="C#" %>
---
+++ ProjetASPNET/ProjetASPNET/Models/Or.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Or
+    {
+        private int idor;
+
+        public int Idor
+        {
+            get { return idor; }
+            set { idor = value; }
+        }
+        private String designationor;
+
+        public String Designationor
+        {
+            get { return designationor; }
+            set { designationor = value; }
+        }
+        private String quantiteor;
+
+        public String Quantiteor
+        {
+            get { return quantiteor; }
+            set { quantiteor = value; }
+        }
+        private String qualiteor;
+
+        public String Qualiteor
+        {
+            get { return qualiteor; }
+            set { qualiteor = value; }
+        }
+        private String destinationor;
+
+        public String Destinationor
+        {
+            get { return destinationor; }
+            set { destinationor = value; }
+        }
+    }
+
+}
