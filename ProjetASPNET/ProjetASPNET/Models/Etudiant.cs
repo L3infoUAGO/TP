32873eec733b8cb0811b5f0bf01d3d255db3778a
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 11:07:39 +0100

    ISHIMWE HAKIZIMANA James Classe Etudiant

---
+++ ProjetASPNET/ProjetASPNET/Models/Etudiant.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Etudiant
+    {
+        public class Etudiant
+        {
+            private int matEt;
+
+            public int MatEt
+            {
+                get { return matEt; }
+                set { matEt = value; }
+            }
+            private string nom;
+
+            public string Nom
+            {
+                get { return nom; }
+                set { nom = value; }
+            }
+            private string postnom;
+
+            public string Postnom
+            {
+                get { return postnom; }
+                set { postnom = value; }
+            }
+            private string prenom;
+
+            public string Prenom
+            {
+                get { return prenom; }
+                set { prenom = value; }
+            }
+
+            public Etudiant()
+            {
+            }
+            public Etudiant(int mat, string nom, string post, string pre)
+            {
+                this.matEt = mat;
+                this.nom = nom;
+                this.postnom = post;
+                this.prenom = pre;
+            }
+        }
+    }
+}
