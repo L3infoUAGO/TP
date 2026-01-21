967560e5906ffb07d7c211d2bcab3d9325ef5ece
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 13:18:25 +0200

    La classe Penalty pour OMBENI NGABONEKA

---
+++ ProjetASPNET/ProjetASPNET/Models/Penalty.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Penalty
+    {
+        public int Id
+        {
+            get;
+        }
+        public string joueur
+        {
+            get;
+        }
+        public string numero
+        {
+            get;
+        }
+        public string penaltyTires
+        {
+            get;
+        }
+        public string penaltyMarques
+        {
+            get;
+            set;
+        }
+    }
+}
