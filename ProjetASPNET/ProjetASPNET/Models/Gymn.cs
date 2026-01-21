42d72da714216eb627a8eaf7aa53c3df0af63f32
resox rx <uagoinfo@gmail.com>
2026-01-21 21:52:48 +0200

    Habimana Joseph

---
+++ ProjetASPNET/ProjetASPNET/Models/Gymn.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Habimana Sebatware Joseph
+public class Gymn
+{
+    
+    public string Nom { get; set; }
+    public string Adresse { get; set; }
+    public int Capacite { get; set; }
+    public List<string> Membres { get; set; }
+
+    
+    public Gymn(string nom, string adresse, int capacite)
+    {
+        Nom = nom;
+        Adresse = adresse;
+        Capacite = capacite;
+        Membres = new List<string>();
+    }
+}
