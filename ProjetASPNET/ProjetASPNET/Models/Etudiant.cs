be2105db3650f999699e7a63e845be16232bb325
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 11:24:43 +0100

    ISHIMWE HAKIZIMANA James Classe Etudiant

--- ProjetASPNET/ProjetASPNET/Models/Etudiant.cs
+++ ProjetASPNET/ProjetASPNET/Models/Etudiant.cs
@@ -7,47 +7,44 @@ namespace ProjetASPNET.Models
 {
     public class Etudiant
     {
-        public class Etudiant
+        private int matEt;
+
+        public int MatEt
         {
-            private int matEt;
+            get { return matEt; }
+            set { matEt = value; }
+        }
+        private string nom;
 
-            public int MatEt
-            {
-                get { return matEt; }
-                set { matEt = value; }
-            }
-            private string nom;
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        private string postnom;
 
-            public string Nom
-            {
-                get { return nom; }
-                set { nom = value; }
-            }
-            private string postnom;
+        public string Postnom
+        {
+            get { return postnom; }
+            set { postnom = value; }
+        }
+        private string prenom;
 
-            public string Postnom
-            {
-                get { return postnom; }
-                set { postnom = value; }
-            }
-            private string prenom;
+        public string Prenom
+        {
+            get { return prenom; }
+            set { prenom = value; }
+        }
 
-            public string Prenom
-            {
-                get { return prenom; }
-                set { prenom = value; }
-            }
-
-            public Etudiant()
-            {
-            }
-            public Etudiant(int mat, string nom, string post, string pre)
-            {
-                this.matEt = mat;
-                this.nom = nom;
-                this.postnom = post;
-                this.prenom = pre;
-            }
+        public Etudiant()
+        {
+        }
+        public Etudiant(int mat, string nom, string post, string pre)
+        {
+            this.matEt = mat;
+            this.nom = nom;
+            this.postnom = post;
+            this.prenom = pre;
         }
     }
 }
