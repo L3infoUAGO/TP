9edc30e032a97324232779b55e8206218f942b6a
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 13:27:06 +0200

    La classe Ordinateur pour STEPHANIE MUSAVULI

---
+++ ProjetASPNET/ProjetASPNET/Models/Ordinateur.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Ordinateur
+    {
+        public int IdOrdi
+        {
+            get;
+        }
+        public string Marque
+        {
+            get;
+        }
+        public string capOrdi
+        {
+            get;
+        }
+
+        public string Prix
+        {
+            get;
+            set;
+        }
+    }
+}
