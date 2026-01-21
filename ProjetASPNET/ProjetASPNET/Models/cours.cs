b9c820f1814a1bcdf0b3ac0faf295928c6991063
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 20:12:19 +0100

    SIFA BIGARUKA Germaine classe cours

---
+++ ProjetASPNET/ProjetASPNET/Models/cours.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class cours
+    {
+        private string codeCours;
+
+        public string CodeCours
+        {
+            get { return codeCours; }
+            set { codeCours = value; }
+        }
+        private string designCours;
+
+        public string DesignCours
+        {
+            get { return designCours; }
+            set { designCours = value; }
+        }
+        private int nbreCred;
+
+        public int NbreCred
+        {
+            get { return nbreCred; }
+            set { nbreCred = value; }
+        }
+
+        public cours()
+        { 
+        }
+        public cours(string code, string des, int nbre)
+        {
+            this.codeCours = code;
+            this.designCours = des;
+            this.nbreCred = nbre;
+        }
+
+    }
+}
