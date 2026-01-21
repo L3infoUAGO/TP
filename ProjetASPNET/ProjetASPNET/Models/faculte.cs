191577b0fbb9e769317c6558fe0095f742e42e91
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 21:12:09 +0100

    Lea Glorieuse classe faculte

---
+++ ProjetASPNET/ProjetASPNET/Models/faculte.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class faculte
+    {
+        private int idfac;
+
+        public int Idfac
+        {
+            get { return idfac; }
+            set { idfac = value; }
+        }
+        private string nomfac;
+
+
+        public string Nomfac
+        {
+            get { return nomfac; }
+            set { nomfac = value; }
+        }
+        private string designfac;
+
+        public string Designfac
+        {
+            get { return designfac; }
+            set { designfac = value; }
+        }
+    }
+}
