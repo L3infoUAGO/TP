5a75bc16e423f519385f0751d2004fb8d8c13c17
resox rx <uagoinfo@gmail.com>
2026-01-21 21:30:42 +0200

    Voici la classe Localisation du CP

---
+++ ProjetASPNET/ProjetASPNET/Models/Localisation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ProjetASPNET.Models
+{
+    // Amani Habiyambere Isaac
+    public class Localisation
+    {
+
+        public int IdPosition { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
+
+
+        public Localisation(int id, double latitude, double longitude, DateTime timestamp)
+        {
+            IdPosition = id;
+            Latitude = latitude;
+            Longitude = longitude;
+            Timestamp = timestamp;
+        }
+
+    }
+}
