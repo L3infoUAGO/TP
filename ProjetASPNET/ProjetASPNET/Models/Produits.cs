048cd3ca62b95d0d6fc7a06c3d6552e45b448751
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 13:44:00 +0200

    La classe produits pour MUBANIO BWIRIZA JEANNE

---
+++ ProjetASPNET/ProjetASPNET/Models/Produits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Produits
+    {
+
+        public int Id_Produit
+        {
+            get;
+        }
+        public string NomProduit
+        {
+            get;
+        }
+
+  
+        public string Prix
+        {
+            get;
+            set;
+        }
+    }
+}
