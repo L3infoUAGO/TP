02e63990522c01690792db4cb695f6ec6239f0c8
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 13:33:50 +0200

    La classe telephone pour MASENGESHO ADOLPHE SIBORUREMA

---
+++ ProjetASPNET/ProjetASPNET/Models/Telephone.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Telephone
+
+    {
+        public int IdTelephone
+        {
+            get;
+        }
+        public string Marque
+        {
+            get;
+        }
+  
+        public string Prix
+        {
+            get;
+            set;
+        }
+    }
+}
