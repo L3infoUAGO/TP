90c732d581c5bb48a7cbdf872df0d50117b872ed
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 13:38:17 +0200

    La classe voiture pour FARIJIKA BUDINGA

---
+++ ProjetASPNET/ProjetASPNET/Models/Voiture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Voiture
+    {
+   
+        public int IdVoiture
+        {
+            get;
+        }
+        public string MarqueVoiture
+        {
+            get;
+        }
+
+        public string Prix
+        {
+            get;
+            set;
+        }
+    }
+}
