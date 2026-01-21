7d41f3ebcb07b9b55b78298a28a804d775f5cde2
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 13:08:13 +0200

    La classe fournisseur pour NICOLAS EFODI

---
+++ ProjetASPNET/ProjetASPNET/Models/Fournisseur.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Fournisseur
+    {
+        public int Id
+        {
+            get;
+        }
+        public string Nom
+        {
+            get;
+        }
+        public string Adresse
+        {
+            get;
+        }
+        public string Telephone
+        {
+            get;
+        }
+        public string Email
+        {
+            get;
+            set;
+        }
+    }
+}
