dd16c4283575112af62235de77003048d2717225
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-21 11:55:53 +0100

    virginie PROMOTION

---
+++ ProjetASPNET/ProjetASPNET/Models/PROMOTION.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class PROMOTION
+    {
+        private string idProm;
+
+        public string IdProm
+        {
+            get { return idProm; }
+            set { idProm = value; }
+        }
+        private string desProm;
+
+        public string DesProm
+        {
+            get { return desProm; }
+            set { desProm = value; }
+        }
+        private string nomProm;
+
+        public string NomProm
+        {
+            get { return nomProm; }
+            set { nomProm = value; }
+        }
+    }
+}
