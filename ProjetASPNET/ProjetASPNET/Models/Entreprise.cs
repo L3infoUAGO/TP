208d45262d04439a2a24edc8d412a7ac1430ebe5
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-22 08:04:01 +0100

    classe Entreprise KASONGO Aline

---
+++ ProjetASPNET/ProjetASPNET/Models/Entreprise.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Entreprise
+    {
+        private string nomEnt;
+
+        public string NomEnt
+        {
+            get { return nomEnt; }
+            set { nomEnt = value; }
+        }
+        private string designEnt;
+
+        public string DesignEnt
+        {
+            get { return designEnt; }
+            set { designEnt = value; }
+        }
+    }
+}
