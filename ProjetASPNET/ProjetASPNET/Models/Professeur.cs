71e1c678afb45df04afbec2b34044dd31f160769
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-22 08:41:04 +0100

    UWAMAHORO MWUNVANEZA Pacifique classe Professeur

---
+++ ProjetASPNET/ProjetASPNET/Models/Professeur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Professeur
+    {
+        private int matriPr;
+
+        public int MatriPr
+        {
+            get { return matriPr; }
+            set { matriPr = value; }
+        }
+        private string nomPr;
+
+        public string NomPr
+        {
+            get { return nomPr; }
+            set { nomPr = value; }
+        }
+        private string postnomPr;
+
+        public string PostnomPr
+        {
+            get { return postnomPr; }
+            set { postnomPr = value; }
+        }
+        private string prenomPr;
+
+        public string PrenomPr
+        {
+            get { return prenomPr; }
+            set { prenomPr = value; }
+        }
+    }
+}
