78cd83e3593f8c5e04f1826ee744c828791f2b01
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-22 12:09:28 +0200

    classe AhadiBahati

---
+++ ProjetASPNET/ProjetASPNET/Models/kinimarche.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class kinimarche
+    {
+    public class Kininmarche
+    {
+        private int IdProduit;
+
+        public int IdProduit1
+        {
+            get { return IdProduit; }
+            set { IdProduit = value; }
+        }
+        private string NomProduit;
+
+        public string NomProduit1
+        {
+            get { return NomProduit; }
+            set { NomProduit = value; }
+        }
+        private string Categorie;
+
+        public string Categorie1
+        {
+            get { return Categorie; }
+            set { Categorie = value; }
+        }
+        private decimal Prix;
+
+        public decimal Prix1
+        {
+            get { return Prix; }
+            set { Prix = value; }
+        }
+        private int Quantite;
+
+        public int Quantite1
+        {
+            get { return Quantite; }
+            set { Quantite = value; }
+        }
+    }
+}
+
+
+    }
