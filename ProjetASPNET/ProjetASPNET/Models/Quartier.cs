77453cf35bfdae9a19f5bf334871c8afb8466b70
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-22 08:32:57 +0200

    Classe Quartier Ass KABANZA

---
+++ ProjetASPNET/ProjetASPNET/Models/Quartier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Quartier
+    {
+        private string designQuart;
+
+        public string DesignQuart
+        {
+            get { return designQuart; }
+            set { designQuart = value; }
+        }
+
+        public Quartier()
+        {
+        }
+
+        public Quartier(string des)
+        {
+            this.designQuart = des;
+        }
+    }
+}
