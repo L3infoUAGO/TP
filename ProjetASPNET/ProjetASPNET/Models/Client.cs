d0255e4b1acfe5b1a9bc81b60759e658b6f71b74
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-22 08:17:23 +0100

    classe Client Mwangaza joaddan

---
+++ ProjetASPNET/ProjetASPNET/Models/Client.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Client
+    {
+        private int idCL;
+
+        public int IdCL
+        {
+            get { return idCL; }
+            set { idCL = value; }
+        }
+        private string nomCl;
+
+        public string NomCl
+        {
+            get { return nomCl; }
+            set { nomCl = value; }
+        }
+        private string adressCL;
+
+        public string AdressCL
+        {
+            get { return adressCL; }
+            set { adressCL = value; }
+        }
+    }
+}
