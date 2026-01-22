a6567216de1d55e4974efd58249792674a8bb61f
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-22 07:55:13 +0100

    jovial ELECTRICITE

---
+++ ProjetASPNET/ProjetASPNET/Models/ELECTRICITE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class ELECTRICITE
+    {
+        private string idElecm;
+
+        public string IdElecm
+        {
+            get { return idElecm; }
+            set { idElecm = value; }
+        }
+        private string design;
+
+        public string Design
+        {
+            get { return design; }
+            set { design = value; }
+        }
+    }
+}
