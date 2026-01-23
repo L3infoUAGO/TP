696899bc5fd7035db1deaf21a008318f77ab6a70
L3InfoUAGO <uagoinfo@gmail.com>
2026-01-23 11:23:44 +0200

    La classe OISEAUX POUR AMANI WENYA LEVIS

---
+++ ProjetASPNET/ProjetASPNET/Models/Oiseaux.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetASPNET.Models
+{
+    public class Oiseaux
+    { private string designOiseau;
+
+    public string designOiseau
+        {
+            get { return designOiseau; }
+            set { designOiseau = value; }
+        }
+
+        public Oiseaux()
+        {
+        }
+
+        public Oiseaux(string des)
+        {
+            this.designOiseau = des;
+        }
+        }
+    }
+
+   
